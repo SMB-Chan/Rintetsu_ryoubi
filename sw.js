3082e92df13146a018f2856acc23b14b15482bd5
Yuki <yuki@example.com>
2026-09-09 21:34:58 +0900

    Fix PWA 404 by pointing to timetable.html instead of index.html

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'timetable-cache-v5';
+const CACHE_NAME = 'timetable-cache-v6';
 const urlsToCache = [
   './',
-  './index.html',
+  './timetable.html',
   './manifest.json',
   './icon.svg'
 ];
